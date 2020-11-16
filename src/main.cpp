1cf69b3b1960e32756d9fbaf1157e2d4c0edd741
fernandez27180509 <jhon.fernandez.27@unsch.edu.pe>
2020-11-16 12:01:42 -0500

    Agregue el menú basico de las operaciones.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <cstdlib>
 
+using namespace std;
+
 int main() {
+	cout << "+--------------------------+" << endl;
+	cout << "|----- C++ calculator -----|" << endl;
+	cout << "+--------------------------+" << endl;
+	cout << "1) Suma" << endl;
+	cout << "2) Resta" << endl;
+	cout << "3) Multiplicacion" << endl;
+	cout << "4) Division" << endl;
+	cout << "5) Potencia" << endl;
+
+	cout << "God be with us." << endl;
 	return EXIT_SUCCESS;
 }
